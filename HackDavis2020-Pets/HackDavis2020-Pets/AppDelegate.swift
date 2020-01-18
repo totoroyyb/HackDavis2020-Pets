f2486326aa26f5a10727b9e4cde400e5e9085a44
failury <failury29@gmail.com>
2020-01-18 15:58:51 -0800

    chat view complete

--- HackDavis2020-Pets/HackDavis2020-Pets/AppDelegate.swift
+++ HackDavis2020-Pets/HackDavis2020-Pets/AppDelegate.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 import CoreData
-
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
--- HackDavis2020-Pets/HackDavis2020-Pets/ContentView.swift
+++ HackDavis2020-Pets/HackDavis2020-Pets/ContentView.swift
@@ -8,20 +8,54 @@
 
 import SwiftUI
 
-struct Chat: Hashable {
-    var userName: String
-    var messageContent: String
+public struct ListSeparatorStyleNoneModifier: ViewModifier {
+    public func body(content: Content) -> some View {
+        content.onAppear {
+            UITableView.appearance().separatorStyle = .none
+        }.onDisappear {
+            UITableView.appearance().separatorStyle = .singleLine
+        }
+    }
 }
-struct ContentView: View {
-    var chatContent = [ Chat(userName: "Me", messageContent: "Hello there"),Chat(userName: "OtherPeople", messageContent: "Yo")]
+extension View {
+    public func listSeparatorStyleNone() -> some View {
+        modifier(ListSeparatorStyleNoneModifier())
+    }
+}
+struct Chat : Hashable {
+    var messageContent: String
+    var userName: String
+    var color: Color
+    var isMe: Bool = false
+}
+
+struct ChatRow : View {
+    var chatMessage: Chat
     var body: some View {
-        VStack{
-            List{
-                ForEach(chatContent,id: \.self){
-                    CC in
-                    Group{
-                        Text(CC.userName)
-                        Text(CC.messageContent)
+        Group {
+            if !chatMessage.isMe {
+                HStack {
+                    Group {
+                        Text(chatMessage.userName)
+                        Text(chatMessage.messageContent)
+                            .bold()
+                            .padding(10)
+                            .foregroundColor(Color.white)
+                            .background(chatMessage.color)
+                            .cornerRadius(10)
+                    }
+                }
+            } else {
+                HStack {
+                    Group {
+                        Spacer()
+                        Text(chatMessage.messageContent)
+                            .bold()
+                            .foregroundColor(Color.white)
+                            .padding(10)
+                            .background(chatMessage.color)
+                            .cornerRadius(10)
+                        Text(chatMessage.userName)
                     }
                 }
             }
@@ -29,8 +63,37 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ContentView : View {
+    @State var composedMessage: String = ""
+    @ObservedObject var chatController: ChatController
+    var body: some View {
+        
+        VStack {
+            List(){
+                ForEach(chatController.messages, id: \.self) { msg in
+                    ChatRow(chatMessage: msg)
+                }
+            }.listSeparatorStyleNone()
+            
+            
+            HStack {
+                TextField("Message...", text: $composedMessage).frame(minHeight: CGFloat(30))
+                Button(action: sendMessage) {
+                    Text("Send")
+                }
+            }.frame(minHeight: CGFloat(50)).padding()
+        }
+    }
+    func sendMessage() {
+        chatController.sendMessage(Chat(messageContent: composedMessage, userName: "C", color: .green, isMe: true))
+        composedMessage = ""
+    }
+}
+
+
+struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(chatController: ChatController())
+            .environmentObject(ChatController())
     }
 }
