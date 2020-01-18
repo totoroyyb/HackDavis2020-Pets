c1d92dabd5b3bfee65a079cf67f09fd3aa0450d4
TotoroQ <totoroq@outlook.com>
2020-01-18 11:47:12 -0800

    First commit

---
+++ HackDavis2020-Pets/HackDavis2020-Pets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackDavis2020-Pets
+//
+//  Created by Quentin on 2020/1/18.
+//  Copyright © 2020 iDEX. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
