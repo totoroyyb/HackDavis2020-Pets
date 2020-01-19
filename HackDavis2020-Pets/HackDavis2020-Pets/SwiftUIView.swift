cc4e3d1e20a1f3ce3cb8fd6bbfb4cc97898e3bb5
failury <failury29@gmail.com>
2020-01-19 02:46:55 -0800

    implemented createpost view

---
+++ HackDavis2020-Pets/HackDavis2020-Pets/SwiftUIView.swift
@@ -0,0 +1,66 @@
+import SwiftUI
+
+struct SwiftUIView: View {
+     @State var text = ""
+
+       var body: some View {
+        VStack {
+           
+            TextView(
+                text: $text
+            )
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        }
+
+       }
+}
+
+struct TextView: UIViewRepresentable {
+    @Binding var text: String
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> UITextView {
+
+        let myTextView = UITextView()
+        myTextView.delegate = context.coordinator
+
+        myTextView.font = UIFont(name: "HelveticaNeue", size: 15)
+        myTextView.isScrollEnabled = true
+        myTextView.isEditable = true
+        myTextView.isUserInteractionEnabled = true
+        myTextView.backgroundColor = UIColor(white: 0.0, alpha: 0.05)
+
+        return myTextView
+    }
+
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.text = text
+    }
+
+    class Coordinator : NSObject, UITextViewDelegate {
+
+        var parent: TextView
+
+        init(_ uiTextView: TextView) {
+            self.parent = uiTextView
+        }
+
+        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            return true
+        }
+
+        func textViewDidChange(_ textView: UITextView) {
+            print("text now: \(String(describing: textView.text!))")
+            self.parent.text = textView.text
+        }
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
